206f1f35e2ec6de7f86615fc9909da281e5169d8
jan van Hest <jan@travpromobile.com>
2024-02-27 13:53:27 +0100

    example classes

--- LingoPartner_console/Program.cs
+++ LingoPartner_console/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LingoPartner_console;
+
+Console.WriteLine("Enter your first name: ");
+string firstName = Console.ReadLine()!;
+Console.WriteLine("Enter your last name: ");
+string lastName = Console.ReadLine()!;
+
+PersonModel person = new PersonModel(firstName, lastName);
+
+Console.WriteLine($"Hello, {person.FirstName} {person.LastName}!");
+
+Console.ReadLine();
