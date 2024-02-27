c746ce8713eea15d44e2eec0c13fb374ee14f7cb
jan van Hest <jan@travpromobile.com>
2024-02-27 14:57:26 +0100

    removed project

--- LingoPartner_console/obj/Debug/net8.0/LingoPartner_console.AssemblyInfo.cs
+++ LingoPartner_console/obj/Debug/net8.0/LingoPartner_console.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LingoPartner_console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6eae6b9e5f143637219e034d77c7201ec087e392")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LingoPartner_console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LingoPartner_console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
