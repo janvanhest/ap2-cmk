a21b867dfbee2da962d825dca5021e649f0ebea8
jan van Hest <jan@travpromobile.com>
2024-02-27 13:53:27 +0100

    example classes

---
+++ LingoPartner_console/PersonModel.cs
@@ -0,0 +1,12 @@
+﻿namespace LingoPartner_console;
+
+public class PersonModel
+{
+  public string FirstName { get; set; }
+  public string LastName { get; set; }
+  public PersonModel(string firstName, string lastName)
+  {
+    FirstName = firstName;
+    LastName = lastName;
+  }
+}
