4a2c048b2dd061cd8c7ba73dad402d29461addcc
jan van Hest <jan@travpromobile.com>
2024-03-20 09:31:28 +0100

    Added first enum to project

--- LingoPartnerConsole/Program.cs
+++ LingoPartnerConsole/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using LingoPartnerDomain;
+using System;
+using LingoPartnerDomain.ENUM;
+
+namespace LingoPartnerApp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to LingoPartnerApp!");
+      //  learningActivityTitle = LearningActivityType.TRUE_FALSE;
+      int learningActivityTitle = (int)LearningActivityType.TRUE_FALSE;
+      Console.WriteLine($"The learning activity title is {learningActivityTitle}");
+      // Your code goes here
+      // Example: Create an instance of a class from LingoPartnerDomain and call its methods
+    }
+  }
+}
+
