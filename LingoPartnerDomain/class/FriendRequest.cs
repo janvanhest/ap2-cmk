4fb727802263396baf54448ec663c8185d0f126f
jan van Hest <jan@travpromobile.com>
2024-03-24 22:57:44 +0100

    Added class FriendRequest.cs

---
+++ LingoPartnerDomain/class/FriendRequest.cs
@@ -0,0 +1,18 @@
+﻿using LingoPartnerDomain.classes;
+using LingoPartnerDomain.enums;
+
+namespace LingoPartnerDomain;
+
+public class FriendRequest
+{
+  public Student Sender { get; set; }
+  public Student Receiver { get; set; }
+  public FriendRequestStatus Status { get; set; }
+
+  public FriendRequest(Student sender, Student receiver)
+  {
+    Sender = sender;
+    Receiver = receiver;
+    Status = FriendRequestStatus.PENDING;
+  }
+}
