466714a738d85f1d5e25aa9a8c93d08018c49aba
jan van Hest <jan@travpromobile.com>
2024-03-28 10:16:24 +0100

    updated small build errors

--- LingoPartnerDomain/class/User.cs
+++ LingoPartnerDomain/class/User.cs
@@ -18,30 +18,37 @@ namespace LingoPartnerDomain
     protected MailAddress Email { get; private set; }
     // Consider a secure way to store password
     [Required, MaxLength(50)]
-    public string userName;
+    protected string UserName;
     // Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character
     [Required, MaxLength(50), RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,15}$")]
     protected string Password;
 
-    public User(Guid id, string firstName, string middleName, string lastName, DateTime dateOfBirth, MailAddress email, string password)
+    public User(Guid id, string firstName, string middleName, string lastName, DateTime dateOfBirth, MailAddress email, string password, string userName)
     {
-      Id = id.Equals(Guid.Empty) ? Guid.NewGuid() : id;
-      this.FirstName = firstName;
-      MiddleName = middleName;
-      LastName = lastName;
-      DateOfBirth = dateOfBirth;
-      Email = email;
-      this.Password = password;
-    }
+      if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || email == null || string.IsNullOrEmpty(password) || string.IsNullOrEmpty(userName))
+      {
+        throw new ArgumentNullException("One or more required fields are empty.");
+      }
+      if (dateOfBirth > DateTime.Now.AddYears(-4))
+      {
+        throw new ArgumentException("User must be at least 4 years old.");
+      }
 
-    public void UpdateProfile(string firstName, string middleName, string lastName, DateTime dateOfBirth, MailAddress email, string password)
-    {
       FirstName = firstName;
       MiddleName = middleName;
       LastName = lastName;
       DateOfBirth = dateOfBirth;
       Email = email;
       Password = password;
+      UserName = userName;
+    }
+
+    public void UpdateUserProfile(string firstName, string middleName, string lastName, DateTime dateOfBirth)
+    {
+      FirstName = firstName;
+      MiddleName = middleName;
+      LastName = lastName;
+      DateOfBirth = dateOfBirth;
     }
 
     public void UpdateName(string firstName, string middleName, string lastName)
@@ -60,10 +67,21 @@ namespace LingoPartnerDomain
     {
       Password = password;
     }
-
+    public void UpdateUserName(string userName)
+    {
+      UserName = userName;
+    }
     public string GetFullName()
     {
       return string.IsNullOrEmpty(MiddleName) ? $"{FirstName} {LastName}" : $"{FirstName} {MiddleName} {LastName}";
+    }
+    public string GetUserName()
+    {
+      return UserName;
+    }
+    public string GetEmail()
+    {
+      return Email.Address;
     }
   }
 }
