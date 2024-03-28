3ddeee9968460c641d0bd2b278bc83779bee2761
jan van Hest <jan@travpromobile.com>
2024-03-28 13:02:29 +0100

    Created first menu option

---
+++ LingoPartnerConsole/Views/Menu.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using LingoPartnerDomain;
+using LingoPartnerDomain.classes;
+
+namespace LingoPartnerConsole.Views
+{
+  internal class Menu
+  {
+    public Administration SchoolAdministration;
+    public Menu(Administration schoolAdministration)
+    {
+      SchoolAdministration = schoolAdministration;
+    }
+    public void Show()
+    {
+      // Two carriage returns
+      Console.WriteLine("\n\n");
+      Console.WriteLine("Welcome to the LingoPartner Console Application");
+      Console.WriteLine("1. Add a teacher");
+
+      // Console.WriteLine("2. Add a student");
+      // Console.WriteLine("3. Add a course");
+      // Console.WriteLine("4. Add a student to a course");
+      // Console.WriteLine("5. Add a teacher to a course");
+      // Console.WriteLine("6. Show all teachers");
+      // Console.WriteLine("7. Show all students");
+      // Console.WriteLine("8. Show all courses");
+      // Console.WriteLine("9. Show all students in a course");
+      // Console.WriteLine("10. Show all teachers in a course");
+      // Console.WriteLine("11. Exit");
+      Console.WriteLine("Please enter your choice:");
+      string choice = Console.ReadLine();
+      switch (choice)
+      {
+        case "1":
+          // AddTeacher();
+          Console.WriteLine("Let's add an teacher!");
+          break;
+        case "11":
+          Environment.Exit(0);
+          break;
+        default:
+          Console.WriteLine("Invalid choice");
+          break;
+      }
+      Console.WriteLine("Press any key to continue...");
+      Console.ReadKey();
+      Show();
+    }
+  }
+}
+
