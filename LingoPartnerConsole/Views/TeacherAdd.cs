a2bffa31b80d4687271bf9071ed12d7330127d82
jan van Hest <jan@travpromobile.com>
2024-04-10 00:10:47 +0200

    made a start with adding teacher

--- LingoPartnerConsole/Views/TeacherAdd.cs
+++ LingoPartnerConsole/Views/TeacherAdd.cs
@@ -1,23 +1,46 @@
 ﻿using LingoPartnerDomain;
 using LingoPartnerDomain.classes;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Net.Mail;
+using LingoPartnerConsole.Classes;
+
 
 namespace LingoPartnerConsole.Views
 {
   internal class TeacherAdd
   {
     private Administration SchoolAdministration;
+
     public TeacherAdd(Administration schoolAdministration)
     {
       SchoolAdministration = schoolAdministration;
     }
     public void Show()
     {
+
       Console.WriteLine("Add a teacher");
+      // Guid id,
+      Guid id = Guid.NewGuid();
+      string firstName = ConsoleHelper.GetStringInput("Enter first name:");
+      string middleName = ConsoleHelper.GetStringInput("Enter middle name:");
+      string lastName = ConsoleHelper.GetStringInput("Enter last name:");
+      DateTime dateOfBirth = ConsoleHelper.GetDateInput();
+      MailAddress email = ConsoleHelper.GetEmailInput();
+      string password = ConsoleHelper.GetPasswordInput();
+      string username = ConsoleHelper.GetStringInput("Enter username:");
+      string department = ConsoleHelper.GetStringInput("Enter department:");
+      Teacher teacher = new Teacher(
+          id,
+          firstName,
+          middleName,
+          lastName,
+          dateOfBirth,
+          email,
+          password,
+          username,
+          department
+      );
+      SchoolAdministration.AddTeacher(teacher);
     }
   }
 }
+
