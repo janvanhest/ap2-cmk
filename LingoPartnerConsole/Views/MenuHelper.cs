96515c6e6419a19af20a42a282525ddd2c361c49
jan van Hest <jan@travpromobile.com>
2024-04-10 00:10:23 +0200

    added menu helper which keeps on displaying menu after loop is executed

---
+++ LingoPartnerConsole/Views/MenuHelper.cs
@@ -0,0 +1,15 @@
+﻿using LingoPartnerDomain;
+
+namespace LingoPartnerConsole.Views
+{
+  internal class MenuHelper
+  {
+    public static void ReturnToMenu(Administration schoolAdministration)
+    {
+      Console.WriteLine("Press any key to return to menu...");
+      Console.ReadKey();
+      Menu menu = new Menu(schoolAdministration);
+      menu.Show();
+    }
+  }
+}
