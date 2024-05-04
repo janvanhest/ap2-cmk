858506d01e4c8a9385533732e9be375ff9bb417a
jan van Hest <jan@travpromobile.com>
2024-05-04 23:31:06 +0200

    created UserAdd and UserShow

---
+++ LingoPartnerConsole/Views/UserList.cs
@@ -0,0 +1,23 @@
+﻿using LingoPartnerConsole.Classes;
+using LingoPartnerDomain.enums;
+using LingoPartnerDomain.classes;
+
+namespace LingoPartnerConsole.Views
+{
+  public class UserList
+  {
+    private Administration SchoolAdministration;
+    public UserList(Administration schoolAdministration)
+    {
+      SchoolAdministration = schoolAdministration;
+    }
+    public void Show()
+    {
+      Console.WriteLine("List of all users");
+      foreach (User user in SchoolAdministration.users)
+      {
+        Console.WriteLine($"{user.FirstName} {user.MiddleName} {user.LastName} ({user.Role})");
+      }
+    }
+  }
+}
