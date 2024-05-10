53ab00a330227b2a136399b1b3a9376aa25d88b9
jan van Hest <jan@travpromobile.com>
2024-05-10 13:13:52 +0200

    created learning module add to add learningModule

---
+++ LingoPartnerConsole/Views/LearningModuleAdd.cs
@@ -0,0 +1,32 @@
+﻿using LingoPartnerConsole.Classes;
+using LingoPartnerDomain.classes;
+
+namespace LingoPartnerConsole
+{
+  public class LearningModuleAdd
+  {
+    private Administration SchoolAdministration;
+
+    public LearningModuleAdd(Administration administration)
+    {
+      SchoolAdministration = administration;
+    }
+
+    public void Show()
+    {
+      Console.WriteLine("Add a new learning module");
+      string name = ConsoleHelper.GetStringInput("Enter name:");
+      string description = ConsoleHelper.GetStringInput("Enter description:");
+
+      // Create the learning module object
+      LearningModule newLearningModule = new LearningModule(
+          name: name,
+          description: description
+      );
+
+      // Add the learning module to the administration
+      SchoolAdministration.Add(newLearningModule);
+      Console.WriteLine($"Learning module {name} successfully added.");
+    }
+  }
+}
