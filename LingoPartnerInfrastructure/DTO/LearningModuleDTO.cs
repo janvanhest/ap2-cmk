71f334accaf5d21b9a17995ef7ccb81184af3dea
jan van Hest <jan@travpromobile.com>
2024-05-10 19:31:45 +0200

    made a start with LearningModule and Learning Activities

---
+++ LingoPartnerInfrastructure/DTO/LearningModuleDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LingoPartnerInfrastructure.DTO
+{
+  public class LearningModuleDTO
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public List<LearningActivityDTO> LearningActivities { get; set; }
+
+    public LearningModuleDTO(int id, string name, string description, List<LearningActivityDTO> learningActivities)
+    {
+      Id = id;
+      Name = name;
+      Description = description;
+      LearningActivities = learningActivities;
+    }
+
+    // public static LearningModuleDTO FromEntity(LearningModule module)
+    // {
+    //   var activityDTOs = module.ShowLearningActivities()
+    //                            .Select(LearningActivityDTO.FromEntity)
+    //                            .ToList();
+
+    //   return new LearningModuleDTO(
+    //       module.Id,
+    //       module.Name,
+    //       module.Description,
+    //       activityDTOs
+    //   );
+    // }
+  }
+}
