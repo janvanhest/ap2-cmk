c03343fcfc2496f73c6b57b3108caba6fdef2046
jan van Hest <jan@travpromobile.com>
2024-05-20 20:58:24 +0200

    Implemented first version of LearningActivityRepository with  NotImplementedExceptions

---
+++ LingoPartnerInfrastructure/Repository/LearningActivityRepository.cs
@@ -0,0 +1,33 @@
+﻿using LingoPartnerDomain;
+using LingoPartnerDomain.classes;
+
+namespace LingoPartnerInfrastructure
+{
+  public class LearningActivityRepository : ILearningActivityRepository
+  {
+    public void AddLearningActivity(LearningActivity activity)
+    {
+      throw new NotImplementedException();
+    }
+
+    public IEnumerable<LearningActivity> GetAllLearningActivities()
+    {
+      throw new NotImplementedException();
+    }
+
+    public LearningActivity GetLearningActivityById(int activityId)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void RemoveLearningActivity(int activityId)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void UpdateLearningActivity(LearningActivity activity)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
