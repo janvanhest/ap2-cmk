23b21e2718b5e4830f0298bc32c0d8ab45747399
jan van Hest <jan@travpromobile.com>
2024-05-22 23:48:49 +0200

    updated functions of ProgressRepository to reflect changed interface

---
+++ LingoPartnerInfrastructure/Repository/ProgressRepository.cs
@@ -0,0 +1,26 @@
+﻿using LingoPartnerDomain.classes;
+using LingoPartnerDomain.interfaces;
+using MySql.Data.MySqlClient;
+
+namespace LingoPartnerInfrastructure.Repository
+{
+  public class ProgressRepository : IProgressRepository
+  {
+    public Progress? AddProgress(Progress progress)
+    {
+      //  TODO: Implement AddProgress
+      throw new NotImplementedException();
+    }
+    // TODO: Implement GetProgressByUserId
+    public IEnumerable<Progress> GetProgressByUserId(int userId)
+    {
+      // TODO: Implement GetProgressByUserId
+      throw new NotImplementedException();
+    }
+    // TODO: Implement UpdateProgress
+    public Progress? UpdateProgress(Progress updateProgress)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
