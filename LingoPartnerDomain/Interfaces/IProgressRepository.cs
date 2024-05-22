a60473a9984fabf55d71e48d2643520f36549fd7
jan van Hest <jan@travpromobile.com>
2024-05-22 23:44:40 +0200

    Created interface for progressRepository

---
+++ LingoPartnerDomain/Interfaces/IProgressRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using LingoPartnerDomain.classes;
+
+namespace LingoPartnerDomain.interfaces
+{
+  public interface IProgressRepository
+  {
+    Progress? AddProgress(Progress progress);
+    Progress? UpdateProgress(Progress progress);
+    void DeleteProgress(int progressId);
+    Progress GetProgressById(int progressId);
+    IEnumerable<Progress> GetAllProgress();
+    IEnumerable<Progress> GetProgressByUserId(int userId);
+    IEnumerable<Progress> GetProgressByLearningActivityId(int learningActivityId);
+  }
+}
