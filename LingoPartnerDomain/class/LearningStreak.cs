2282c1db9c70f30c88bf7b8f5e908529a5e31dc5
jan van Hest <jan@travpromobile.com>
2024-05-30 13:12:19 +0200

    added learning streak class

---
+++ LingoPartnerDomain/class/LearningStreak.cs
@@ -0,0 +1,15 @@
+﻿namespace LingoPartnerDomain.classes
+{
+  public class LearningStreak
+  {
+    public DateTime StartDate { get; private set; }
+    public DateTime EndDate { get; private set; }
+    public int Length => (EndDate - StartDate).Days + 1;
+
+    public LearningStreak(DateTime startDate, DateTime endDate)
+    {
+      StartDate = startDate;
+      EndDate = endDate;
+    }
+  }
+}
