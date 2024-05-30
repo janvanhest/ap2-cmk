66465bdbc6f6a50151f2a862b8f43cb564015792
jan van Hest <jan@travpromobile.com>
2024-05-30 15:10:35 +0200

    Added GetAllProgress to progressrepo and corresponding interface

--- LingoPartnerDomain/Interfaces/IProgressRepository.cs
+++ LingoPartnerDomain/Interfaces/IProgressRepository.cs
@@ -9,7 +9,8 @@ namespace LingoPartnerDomain.interfaces
     Progress? UpdateProgress(Progress updateProgress);
     // void DeleteProgress(int progressId);
     // Progress GetProgressById(int progressId);
-    // IEnumerable<Progress> GetAllProgress();
+    IEnumerable<Progress> GetAllProgress();
     IEnumerable<Progress> GetProgressByUserId(int userId);
+
   }
 }
