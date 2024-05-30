9c5e1d0d930755a5730eacec14268fd8179038dc
jan van Hest <jan@travpromobile.com>
2024-05-30 19:50:45 +0200

    Added Lengt and points to LearningStreak Class

--- LingoPartnerDomain/class/LearningStreak.cs
+++ LingoPartnerDomain/class/LearningStreak.cs
@@ -1,15 +1,17 @@
-﻿namespace LingoPartnerDomain.classes
+﻿public class LearningStreak
 {
-  public class LearningStreak
-  {
-    public DateTime StartDate { get; private set; }
-    public DateTime EndDate { get; private set; }
-    public int Length => (EndDate - StartDate).Days + 1;
+  public int UserId { get; }
+  public DateTime StartDate { get; }
+  public DateTime EndDate { get; }
+  public int Length { get; }
+  public int Points { get; }
 
-    public LearningStreak(DateTime startDate, DateTime endDate)
-    {
-      StartDate = startDate;
-      EndDate = endDate;
-    }
+  public LearningStreak(int userId, DateTime startDate, DateTime endDate, int length, int points)
+  {
+    UserId = userId;
+    StartDate = startDate;
+    EndDate = endDate;
+    Length = length;
+    Points = points;
   }
 }
