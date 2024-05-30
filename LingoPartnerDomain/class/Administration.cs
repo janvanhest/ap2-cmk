ade5ddaeb7beb2a45c08dc3559a6cd8f5ab5d1ae
jan van Hest <jan@travpromobile.com>
2024-05-30 15:48:50 +0200

    Removed RetrieveAllData to get rid of yellow lines.

--- LingoPartnerDomain/class/Administration.cs
+++ LingoPartnerDomain/class/Administration.cs
@@ -9,6 +9,7 @@ namespace LingoPartnerDomain.classes
     private readonly IUserRepository userRepository;
     private readonly ILearningModuleRepository learningModuleRepository;
     private readonly ILearningActivityRepository learningActivityRepository;
+    private readonly IProgressRepository progressRepository;
 
     private List<User> users;
     public IReadOnlyList<User> Users => users;
@@ -22,14 +23,20 @@ namespace LingoPartnerDomain.classes
     public Administration(
       IUserRepository userRepository,
       ILearningModuleRepository learningModuleRepository,
-      ILearningActivityRepository learningActivityRepository)
+      ILearningActivityRepository learningActivityRepository,
+      IProgressRepository progressRepository
+      )
     {
       // Inject the repositories
       this.userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
       this.learningModuleRepository = learningModuleRepository ?? throw new ArgumentNullException(nameof(learningModuleRepository));
       this.learningActivityRepository = learningActivityRepository ?? throw new ArgumentNullException(nameof(learningActivityRepository));
+      this.progressRepository = progressRepository ?? throw new ArgumentNullException(nameof(progressRepository));
+      this.users = userRepository.GetUsers().ToList();
+      this.learningModules = learningModuleRepository.GetAllLearningModules().ToList();
+      this.learningActivities = learningActivityRepository.GetAll().ToList();
       // Get the data from the repositories
-      RetrieveAllData();
+      // RetrieveAllData();
     }
     public void Add(User user)
     {
@@ -109,7 +116,7 @@ namespace LingoPartnerDomain.classes
     {
       this.users = userRepository.GetUsers().ToList();
       this.learningModules = learningModuleRepository.GetAllLearningModules().ToList();
-      this.learningActivities = learningActivityRepository.GetAllLearningActivities().ToList();
+      this.learningActivities = learningActivityRepository.GetAll().ToList();
     }
 
     public LearningActivity? Add(LearningActivity newLearningActivity)
@@ -117,13 +124,15 @@ namespace LingoPartnerDomain.classes
       var module = learningModules.Find(m => m.Id == newLearningActivity.LearningModuleId);
       if (module != null)
       {
-        LearningActivity result = learningActivityRepository.AddLearningActivity(newLearningActivity);
+        // FIXME: Why would a adding a learning activity return null?
+        // 
+        LearningActivity? result = learningActivityRepository.Add(newLearningActivity);
         if (result == null)
         {
           LoggingHelper.LogError(new Exception("Learning activity not added."), "Learning activity not added.");
           return null;
         }
-        this.learningActivities = learningActivityRepository.GetAllLearningActivities().ToList();
+        this.learningActivities = learningActivityRepository.GetAll().ToList();
         return result;
       }
       else
@@ -131,6 +140,77 @@ namespace LingoPartnerDomain.classes
         LoggingHelper.LogWarning("Learning module not found.");
         return null;
       }
+    }
+    public int CalculateTotalScore(User user)
+    {
+      int userId = user.Id ?? throw new ArgumentNullException(nameof(user.Id));
+      List<Progress> progressRecords = progressRepository.GetProgressByUserId(userId).ToList();
+      var streaks = CalculateStreaks(progressRecords);
+
+      // Choose the strategy
+      IStreakCalculationStrategy strategy = new AdvancedStreakCalculationStrategy();
+      StreakCalculator calculator = new StreakCalculator(strategy);
+
+      return calculator.CalculateScore(streaks);
+    }
+
+    public LearningStreak? GetLastStreak(User user)
+    {
+      List<Progress> progressRecords = GetProgressRecordsByUser(user);
+      List<LearningStreak> streaks = CalculateStreaks(progressRecords);
+      return streaks.OrderByDescending(streak => streak.EndDate).FirstOrDefault();
+    }
+
+    public LearningStreak? GetFirstStreak(User user)
+    {
+      List<Progress> progressRecords = GetProgressRecordsByUser(user);
+      var streaks = CalculateStreaks(progressRecords);
+      return streaks.OrderBy(streak => streak.StartDate).FirstOrDefault();
+    }
+
+    public List<Progress> GetProgressRecordsByUser(User user)
+    {
+      int userId = user.Id ?? throw new ArgumentNullException(nameof(user.Id));
+      return progressRepository.GetProgressByUserId(userId).ToList();
+    }
+    private List<LearningStreak> CalculateStreaks(List<Progress> progressRecords)
+    {
+      var streaks = new List<LearningStreak>();
+      if (progressRecords == null || progressRecords.Count == 0)
+        return streaks;
+
+      progressRecords.Sort((x, y) => x.Date.CompareTo(y.Date));
+
+      DateTime? streakStart = null;
+      DateTime? streakEnd = null;
+
+      for (int i = 0; i < progressRecords.Count; i++)
+      {
+        if (streakStart == null)
+        {
+          streakStart = progressRecords[i].Date;
+          streakEnd = progressRecords[i].Date;
+        }
+        else
+        {
+          if ((progressRecords[i].Date - streakEnd.Value).Days == 1)
+          {
+            streakEnd = progressRecords[i].Date;
+          }
+          else
+          {
+            streaks.Add(new LearningStreak(streakStart.Value, streakEnd.Value));
+            streakStart = progressRecords[i].Date;
+            streakEnd = progressRecords[i].Date;
+          }
+        }
+      }
+      if (streakStart != null && streakEnd != null)
+      {
+        streaks.Add(new LearningStreak(streakStart.Value, streakEnd.Value));
+      }
+
+      return streaks;
     }
   }
 }
