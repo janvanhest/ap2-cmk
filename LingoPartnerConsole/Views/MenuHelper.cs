6a3fa6685adc216a3e3f77ae05ab3bb789f5d777
jan van Hest <jan@travpromobile.com>
2024-06-04 22:57:14 +0200

    Moved LearningStreakService to menuhelper

--- LingoPartnerConsole/Views/MenuHelper.cs
+++ LingoPartnerConsole/Views/MenuHelper.cs
@@ -1,15 +1,19 @@
 ﻿using LingoPartnerConsole.Helpers;
 using LingoPartnerDomain.Classes;
+using LingoPartnerDomain.Interfaces.Services;
 
 namespace LingoPartnerConsole.Views
 {
   internal class MenuHelper
   {
-    public static void ReturnToMenu(Administration schoolAdministration)
+    public static void ReturnToMenu(
+        Administration schoolAdministration,
+        ILearningStreakService learningStreakService)
     {
+
       ConsoleHelper.DisplayTypingAnimation("Press any key to return to the menu...");
       Console.ReadKey();
-      Menu menu = new Menu(schoolAdministration);
+      Menu menu = new(schoolAdministration, learningStreakService);
       menu.Show();
     }
   }
