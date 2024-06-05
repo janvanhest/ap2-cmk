ea878a755230d6c4e362e93a321a0de4b63cc211
jan van Hest <jan@travpromobile.com>
2024-06-05 21:08:23 +0200

    Created LearningStreak Class

--- LingoPartnerDomain/classes/LearningStreak.cs
+++ LingoPartnerDomain/classes/LearningStreak.cs
@@ -2,23 +2,10 @@
 {
   public class LearningStreak
   {
-    public LearningStreak()
-    {
-
-    }
-
-    internal int GetLength()
-    {
-    }
-
-    internal DateTime getFirstDay()
-    {
-      throw new NotImplementedException();
-    }
-
-    internal DateTime getLastDay()
-    {
-      throw new NotImplementedException();
-    }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public List<DateTime> ActivityDates { get; set; } = new List<DateTime>();
+    public int Length => (EndDate - StartDate).Days + 1;
+    public int Score => Length * 10; // Example scoring: 10 points per day
   }
 }
