c57aa4e44767ca9326c7702445f8d83b006fa4dd
jan van Hest <jan@travpromobile.com>
2024-06-06 12:47:57 +0200

    Implemented basic functions corresponding to ILearningStreakService

--- LingoPartnerInfrastructure/Services/LearningStreakService.cs
+++ LingoPartnerInfrastructure/Services/LearningStreakService.cs
@@ -1,52 +1,73 @@
 ﻿using LingoPartnerDomain.Classes;
 using LingoPartnerDomain.Interfaces.Repositories;
 using LingoPartnerDomain.Interfaces.Services;
-using LingoPartnerDomain.Interfaces.Services.Strategy;
+using Org.BouncyCastle.Asn1.Misc;
 
 namespace LingoPartnerInfrastructure.Services
 {
   public class LearningStreakService : ILearningStreakService
   {
     private readonly IProgressRepository progressRepository;
+    private readonly IAuthenticationService authenticationService;
+    private User user;
 
-    public LearningStreakService(IProgressRepository progressRepository)
+    public LearningStreakService(
+      IProgressRepository progressRepository,
+      IAuthenticationService authenticationService
+    )
     {
       this.progressRepository = progressRepository;
+      this.authenticationService = authenticationService;
+      this.user = authenticationService.CurrentUser ?? throw new ArgumentNullException(nameof(authenticationService));
+    }
+    private List<Progress> GetProgressItems()
+    {
+      int userId = user.Id ?? throw new ArgumentNullException(nameof(user));
+      return progressRepository.GetProgressByUserId(userId).ToList();
+    }
+    public List<LearningStreak> GetLearningStreaks()
+    {
+      List<LearningStreak> streaks = new List<LearningStreak>();
+      var progressItems = GetProgressItems();
+      if (!progressItems.Any()) return streaks;
+
+      LearningStreak? currentStreak = null;
+
+      for (int i = 0; i < progressItems.Count; i++)
+      {
+        var progress = progressItems[i];
+
+        if (currentStreak == null)
+        {
+          currentStreak = new LearningStreak();
+          currentStreak.AddActivityDate(progress.Date);
+          streaks.Add(currentStreak);
+        }
+        else
+        {
+          var previousProgress = progressItems[i - 1];
+          bool isNextDay = (progress.Date - previousProgress.Date).Days == 1;
+          bool isWeekendSkip = previousProgress.Date.DayOfWeek == DayOfWeek.Friday && progress.Date.DayOfWeek == DayOfWeek.Sunday;
+
+          if (isNextDay || isWeekendSkip)
+          {
+            currentStreak.AddActivityDate(progress.Date);
+          }
+          else
+          {
+            currentStreak.AddActivityDate(progress.Date);
+            streaks.Add(currentStreak);
+          }
+        }
+      }
+
+      return streaks;
     }
 
-    public List<LearningStreak> GetAdvancedLearningStreaks(int userId)
+    public int CalculateTotalScore()
     {
-      throw new NotImplementedException();
+      return GetLearningStreaks().Sum(streak => streak.Score);
     }
-
-    public int GetCurrentLearningStreak()
-    {
-      throw new NotImplementedException();
-    }
-
-    public List<LearningStreak> GetLearningStreaks(int userId)
-    {
-      throw new NotImplementedException();
-    }
-
-    public IReadOnlyCollection<LearningStreak> GetStreaks()
-    {
-      throw new NotImplementedException();
-    }
-
-    public int GetTotalScore(int userId, ILearningStreakCalculationStrategy calculator)
-    {
-      throw new NotImplementedException();
-    }
-
-    public IEnumerable<DateTime> GetUniqueActivityDates(int userId)
-    {
-      throw new NotImplementedException();
-    }
-
-    public void Initialize(User user)
-    {
-      throw new NotImplementedException();
-    }
+    public LearningStreak? GetCurrentStreak() => GetLearningStreaks().LastOrDefault();
   }
 }
