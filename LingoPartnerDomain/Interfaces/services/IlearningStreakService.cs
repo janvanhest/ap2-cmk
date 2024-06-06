4fef2d2371e9bd07ef65bbf8ab53f99a5cceee82
jan van Hest <jan@travpromobile.com>
2024-06-06 12:48:13 +0200

    Updated ILearningStreakService

--- LingoPartnerDomain/Interfaces/services/IlearningStreakService.cs
+++ LingoPartnerDomain/Interfaces/services/IlearningStreakService.cs
@@ -1,17 +1,11 @@
 ﻿using LingoPartnerDomain.Classes;
-using LingoPartnerDomain.Interfaces.Services.Strategy;
 
 namespace LingoPartnerDomain.Interfaces.Services
 {
   public interface ILearningStreakService
   {
-    void Initialize(User user);
-    int GetCurrentLearningStreak();
-    int GetTotalScore(int userId, ILearningStreakCalculationStrategy calculator);
-    IReadOnlyCollection<LearningStreak> GetStreaks();
-    IEnumerable<DateTime> GetUniqueActivityDates(int userId);
-    List<LearningStreak> GetLearningStreaks(int userId);
-    List<LearningStreak> GetAdvancedLearningStreaks(int userId);
-
+    List<LearningStreak> GetLearningStreaks();
+    int CalculateTotalScore();
+    LearningStreak? GetCurrentStreak();
   }
 }
