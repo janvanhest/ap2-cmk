7cf944eadbdeaf0de4b2b8697f88dcae0d6c403b
jan van Hest <jan@travpromobile.com>
2024-06-18 21:18:24 +0200

    Added GetModuleCompletionPercentage for dashboard

--- LingoPartnerDomain/Services/ProgressService.cs
+++ LingoPartnerDomain/Services/ProgressService.cs
@@ -1,16 +1,24 @@
 ﻿using LingoPartnerDomain.Classes;
+using LingoPartnerDomain.enums;
 using LingoPartnerDomain.Interfaces.Repositories;
 using LingoPartnerDomain.Interfaces.Services;
+using LingoPartnerInfrastructure.Services;
+using LingoPartnerShared.Helpers;
 
 namespace LingoPartnerDomain.Services
 {
   public class ProgressService : IProgressService
   {
     private readonly IProgressRepository _progressRepository;
+    private readonly ILearningActivityService _learningActivityService;
 
-    public ProgressService(IProgressRepository progressRepository)
+    public ProgressService(
+      IProgressRepository progressRepository,
+      ILearningActivityService learningActivityService
+      )
     {
-      _progressRepository = progressRepository ?? throw new ArgumentNullException(nameof(progressRepository));
+      _progressRepository = progressRepository;
+      _learningActivityService = learningActivityService;
     }
 
     public Progress? AddProgress(Progress progress)
@@ -36,6 +44,22 @@ namespace LingoPartnerDomain.Services
     public IEnumerable<DateTime> GetUniqueDatesByUserId(int userId)
     {
       return _progressRepository.GetUniqueDatesByUserId(userId);
+    }
+
+    public double GetModuleCompletionPercentage(int moduleId, User? user)
+    {
+
+      User? currentUser = user ?? throw new ArgumentNullException(nameof(user));
+      int? userId = currentUser.Id ?? throw new ArgumentNullException(nameof(user.Id));
+      IEnumerable<LearningActivity> activitiesByLearningModuleId = _learningActivityService.GetLearningActivitiesByModuleId(moduleId).ToArray();
+      if (activitiesByLearningModuleId.Count() == 0) return 0;
+
+      var completedActivities = _progressRepository
+          .GetProgressByUserId(userId.Value)
+          .Where(p => p.Status == ProgressStatus.COMPLETED && activitiesByLearningModuleId.Any(la => la.Id == p.LearningActivityId))
+          .Count();
+
+      return completedActivities * 100.0 / activitiesByLearningModuleId.Count();
     }
   }
 }
